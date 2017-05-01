32778faad5e77e49b9ad1d89e25a362cfecf442b
Joost <MinorJoost@users.noreply.github.com>
2017-05-01 11:15:00 +0200

    Index

--- lab/week3/convertCSV2JSON.py
+++ lab/week3/convertCSV2JSON.py
@@ -1,10 +1,12 @@
 import csv
 import json
 
-csvfile = open('data.csv', 'r')
+csvfile = open('tc_data_events.csv', 'r')
 jsonfile = open('jsonfile.json', 'w')
 
-fieldnames = ("month", "rain")
+fieldnames = ("id",	"message",	"reprocess",	"deveui",	"fcntup",	"spfact", "devicename",	"time",
+              "event", "type", "status", "decoded", "time_unix")
+
 reader = csv.DictReader(csvfile, fieldnames)
 out = json.dumps([row for row in reader])
 jsonfile.write(out)
